3536f2ba723d88966d2ca0122aea13dff93a4592
Yu Shi <yushiys95@gmail.com>
2021-04-06 00:52:55 -0400

    Done.

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Article({title, date, preview}){
+    return(
+        <article>
+        <h3>{title}</h3>
+        <small>{date}</small>
+        <p>{preview}</p>
+        </article>
+    );
+}
+
+
+export default Article;
